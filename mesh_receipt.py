ed7cc7d815e98becd18770653bdb555c56de29e9
dan <dan@dmonline.uk>
2025-07-07 12:32:32 +0100

    added exec_sql and test_exec_sql

--- mesh_receipt.py
+++ mesh_receipt.py
@@ -9,6 +9,7 @@ from socket import gethostname
 
 
 PRIVACY_SERVICE_URL = 'http://127.0.0.1:8005'
+load_dotenv()
 
 
 def upload_ndoo_response(exec_id: str, filepath: str):
@@ -18,6 +19,26 @@ def upload_ndoo_response(exec_id: str, filepath: str):
     else:
         response = requests.post(url, files = {'file': open(filepath, 'r')})
     return response
+
+
+def get_conn_string():
+    conn_string = 'UID=%s;PWD=%s;DATABASE=%s;SERVER=%s;DRIVER=%s' % (
+            os.getenv('MESH_PROCESSING_USER'),
+            os.getenv('MESH_PROCESSING_PASSWORD'),
+            'full-dfms-medal-pcdchecker',
+            os.getenv('SQL_SERVER'),
+            '{ODBC Driver 18 for SQL Server}'
+        )
+    return conn_string
+
+
+def exec_sql(conn_string: str, sql_statement: str):
+    with pyodbc.connect(conn_string) as connection:
+        connection.autocommit = True
+        cursor = connection.cursor()
+        cursor.execute(sql_statement)
+        results = cursor.fetchall() # list of tuples
+    return results
 
 
 if __name__ == '__main__':
--- test_mesh_receipt.py
+++ test_mesh_receipt.py
@@ -2,3 +2,12 @@ from mesh_receipt import *
 
 def test_upload_ndoo_response():
     assert upload_ndoo_response('1234567890','1111111111,\n') == 'true'
+
+def test_get_conn_string():
+    os.environ['MESH_PROCESSING_USER'] = 'TEST'
+    os.environ['MESH_PROCESSING_PASSWORD'] = 'TEST'
+    os.environ['SQL_SERVER'] = 'TEST'
+    assert get_conn_string() == 'UID=TEST;PWD=TEST;DATABASE=full-dfms-medal-pcdchecker;SERVER=TEST;DRIVER={ODBC Driver 18 for SQL Server}'
+
+def test_exec_sql():
+    assert exec_sql(get_conn_string(), "select 'true';")[0][0] == 'true'
